24edd3f75ebe79df13563c1638620f1a2e018a45
Ivan Ogloblin <vip.ogloblin@bk.ru>
2024-03-23 23:17:04 +0800

    remmove wf controller and add two changes

---
+++ Api_mobile/Controllers/UserDevicesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Api_mobile.Models;
+
+namespace Api_mobile.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserDevicesController : ControllerBase
+    {
+        private readonly My_dbContext _dbContext;
+
+
+        [HttpGet]
+        public IActionResult Test(int userId)
+        {
+            var UserDevices = _dbContext.Users
+                .Where(d => d.IdUser == userId)
+                .SelectMany(d => d.User)
+            // Замените на соответствующую логику, возвращающую данные
+            return Ok();
+        }
+    }
+}
